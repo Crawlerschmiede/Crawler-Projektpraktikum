eeb69c388c48cd0ea7e51ce13985c494d0257801
Squeemble <jbrockmann@ewe.net>
2025-12-10 16:11:22 +0100

    added very basic roam function to test movement for enemy

--- scripts/enemy_vampire_bat.gd
+++ scripts/enemy_vampire_bat.gd
@@ -2,11 +2,31 @@ class_name EnemyVampireBat
 
 extends MoveableEntity
 
+var roam_timer :float = 5.0
+const roam_cooldown: int = 2
+func roam(delta):
+	roam_timer-=delta
+	var direction_int = 0
+	var direction =Vector2i.ZERO
+	if roam_timer<=0:
+		direction_int = rng.randi_range(0, 3)
+		
+		if direction_int == 0:
+			direction = Vector2i.RIGHT
+		elif direction_int == 1:
+			direction = Vector2i.LEFT
+		elif direction_int == 2:
+			direction = Vector2i.UP
+		elif direction_int == 3:
+			direction = Vector2i.DOWN
+		move_to_tile(direction)
+		roam_timer=roam_cooldown
 
 func _ready() -> void:
 	super_ready("enemy_flying")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	sprite.play("default")
+	roam(delta)
