f696dc1affaad135510c8dc4b64b5c17f2424bba
m-weida <117124336+m-weida@users.noreply.github.com>
2026-01-19 13:06:47 +0100

    refactor: fix linting errors

--- scenes/rooms/Scripts/RoomTemplate.gd
+++ scenes/rooms/Scripts/RoomTemplate.gd
@@ -3,10 +3,11 @@ extends Node2D
 @export var spawn_chance: float = 1.0
 @export var max_count: int = 999
 @export var min_rooms_before_spawn: int = 0
-@onready var tilemap = $TileMapLayer
-@onready var doors = $Doors.get_children()
 @export var is_corridor := true
 @export var required_min_count: int = 0
+
+@onready var tilemap = $TileMapLayer
+@onready var doors = $Doors.get_children()
 
 
 func get_free_doors():
