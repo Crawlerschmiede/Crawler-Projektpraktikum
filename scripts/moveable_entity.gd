0a73a74f3de4c826cd197a16e125d114d79be4d2
Leister <Ole.Leister@telekom.de>
2026-01-30 18:05:47 +0100

    refog addaptet

--- scripts/moveable_entity.gd
+++ scripts/moveable_entity.gd
@@ -336,23 +336,17 @@ func get_line_cells(start: Vector2i, end: Vector2i) -> Array:
 
 	var sx = 1 if x0 < x1 else -1
 	var sy = 1 if y0 < y1 else -1
-
 	var err = dx - dy
 
 	while true:
 		points.append(Vector2i(x0, y0))
-
 		if x0 == x1 and y0 == y1:
 			break
-
 		var e2 = err * 2
-
 		if e2 > -dy:
 			err -= dy
 			x0 += sx
-
 		if e2 < dx:
 			err += dx
 			y0 += sy
-
 	return points
