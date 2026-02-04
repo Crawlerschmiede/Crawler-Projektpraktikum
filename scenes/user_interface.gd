6cff4aebc031fd9a5026114b7af4a57a31bcce83
Leister <Ole.Leister@telekom.de>
2026-02-04 20:32:25 +0100

    fix equipment

--- scenes/user_interface.gd
+++ scenes/user_interface.gd
@@ -9,31 +9,33 @@ var merchant_in_range: bool = false
 @onready var equipmentlabel := $Inventory/Inner/EquiptmentLabel
 @onready var player := $".."
 @onready var merchantgui := $Inventory/Inner/MerchantContainer/HBoxContainer/VBoxContainer
-
+@onready var merchantgui_MerchantContainer := $Inventory/Inner/MerchantContainer
 @onready var coin_screen = $Inventory/price
+
+func _enable_merchant_ui():
+	merchantgui_MerchantContainer.visible = true
+	_set_mouse_filter_recursive(merchantgui_MerchantContainer, Control.MOUSE_FILTER_STOP)
+
+	equipment.visible = false
+	equipmentlabel.visible = false
+
+
+func _disable_merchant_ui():
+	merchantgui_MerchantContainer.visible = false
+	_set_mouse_filter_recursive(merchantgui_MerchantContainer, Control.MOUSE_FILTER_IGNORE)
+
+	equipment.visible = true
+	equipmentlabel.visible = true
 
 
 func _input(event):
 	if event.is_action_pressed("open_inventory"):
 		$Inventory/Inner.visible = !$Inventory/Inner.visible
 
-	# Show merchant UI only when player is inside merchant Area2D
-	if merchant_in_range:
-		equipment.visible = false
-		equipmentlabel.visible = false
-		merchantgui.visible = true
-	else:
-		equipment.visible = true
-		equipmentlabel.visible = true
-		merchantgui.visible = false
-
-	# Hotbar 1..5 -> SlotIndex 13..17
 	for i in range(1, 6):
 		if event.is_action_pressed("hotbar_slot%d" % i) and not $Inventory/Inner.visible:
 			var slot_index := i + 17
 			PlayerInventory.set_selectet_slot(slot_index)
-			#print("Slot:", slot_index, "selected!")
-
 			_refresh_hotbar_styles()
 			return
 
@@ -47,6 +49,8 @@ func _refresh_hotbar_styles() -> void:
 
 func _ready() -> void:
 	# Connect coin display to PlayerInventory changes
+	_disable_merchant_ui()
+
 	if (
 		typeof(PlayerInventory) != TYPE_NIL
 		and PlayerInventory != null
@@ -89,7 +93,7 @@ func _on_merchant_open(entity, data):
 	# remember which merchant we're interacting with
 	current_merchant = entity
 	merchant_in_range = true
-	merchantgui.get_parent().visible = true
+	_enable_merchant_ui()
 
 	# Diagnostics: ensure data contains items
 	if typeof(data) != TYPE_DICTIONARY:
@@ -141,11 +145,14 @@ func _on_merchant_updated(updated):
 	#print("rebuild Merchant")
 	merchantgui._rebuild(updated)
 
+func _set_mouse_filter_recursive(node: Node, filter: int) -> void:
+	if node is Control:
+		(node as Control).mouse_filter = filter
+	for c in node.get_children():
+		_set_mouse_filter_recursive(c, filter)
 
-func _on_merchant_left(entity: Node) -> void:
-	#print("UI: merchant left:", entity)
-	if current_merchant == entity:
-		current_merchant = null
-		merchant_in_range = false
-		merchantgui.clear()
-		merchantgui.get_parent().visible = false
+func _on_merchant_left(_entity = null) -> void:
+	current_merchant = null
+	merchant_in_range = false
+	merchantgui.clear()
+	_disable_merchant_ui()
