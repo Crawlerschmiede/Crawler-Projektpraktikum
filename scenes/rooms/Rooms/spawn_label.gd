d21fcbf286bdb488b2201a599be9581e460c702f
Templer08 <lucas.alisch@stud-provadis-hochschule.de>
2026-02-05 18:18:23 +0100

    lint error fix

--- scenes/rooms/Rooms/spawn_label.gd
+++ scenes/rooms/Rooms/spawn_label.gd
@@ -19,5 +19,5 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		if label_spawn.visible== true:
-			print("Player left tutorial room, in which he was before.")	
+			print("Player left tutorial room, in which he was before.")
 		label_spawn.visible = false
