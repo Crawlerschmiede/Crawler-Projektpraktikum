e38393d77cc26cfa3e682f9da3f5a5931a4f403b
Parya Khalili Motlagh <parya.khalili-motlagh-kasmaei@telekom.de>
2026-02-16 16:33:44 +0100

    Added skill menue with 8 skills. skills have icon and info text. panels have hover and select effect

---
+++ scripts/UI/glow.gd
@@ -0,0 +1,40 @@
+extends Panel
+
+var glow_shader = preload("res://shaders/glitterglow.gdshader")
+var select_texture = preload("res://assets/menu/Frames/Ram Deco All.png")
+
+var select_layer: TextureRect
+var is_selected: bool = false
+
+func _ready():
+
+	select_layer = TextureRect.new()
+	select_layer.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	select_layer.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	select_layer.texture = select_texture
+	select_layer.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	select_layer.stretch_mode = TextureRect.STRETCH_SCALE
+	 
+	select_layer.material = CanvasItemMaterial.new()
+	select_layer.material.blend_mode = CanvasItemMaterial.BLEND_MODE_ADD
+	
+	select_layer.visible = false
+	add_child(select_layer)
+
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	gui_input.connect(_on_gui_input)
+
+func _on_mouse_entered():
+	self.material = ShaderMaterial.new()
+	self.material.shader = glow_shader
+
+func _on_mouse_exited():
+	self.material = null
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			is_selected = !is_selected
+			select_layer.visible = is_selected
+			self.material = null
