2802daf5a79898c4fe4f864651d51c84d9697fa8
m-weida <117124336+m-weida@users.noreply.github.com>
2026-02-20 13:34:10 +0100

    feat: implement tooltips wiring for skill display and descriptions

--- scripts/UI/skill_tooltip.gd
+++ scripts/UI/skill_tooltip.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var popup_panel: PopupPanel = %SkillTooltip
+@onready var tooltip_label: Label = $UI/SkillTooltip/VBoxContainer/Label
 
 
 func _ready():
@@ -12,10 +13,21 @@ func _process(_delta):
 		popup_panel.position = get_global_mouse_position() + Vector2(5, 5)
 
 
-func skill_tooltip(_slot, _item):
+func show_tooltip(skill_name: String, skill_description: String):
+	if tooltip_label != null:
+		var shown_name = skill_name.strip_edges()
+		var shown_description = skill_description.strip_edges()
+		if shown_name == "":
+			tooltip_label.text = shown_description
+		else:
+			tooltip_label.text = shown_name + "\n" + shown_description
 	popup_panel.popup()
 	popup_panel.position = get_global_mouse_position() + Vector2(5, 5)
 
 
 func hide_skill_tolltip():
 	popup_panel.hide()
+
+
+func skill_tooltip(_slot, _item):
+	show_tooltip("", "")
